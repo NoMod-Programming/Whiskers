86f2201eab7ae9aa305be6989a1ac3068db28944
Hazel Pedemonte <epedemont@protonmail.ch>
2017-11-05 21:56:11 -0800

    Update to 0.9a6

--- crToCrlf.py
+++ crToCrlf.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 import sys
 filename = sys.argv[1]
+print("Converting {}...".format(filename))
 text = open(filename, 'rb').read().replace('\r','\r\n')
 open(filename,'wb').write(text)
