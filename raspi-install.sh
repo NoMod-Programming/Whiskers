2b0ef8f8667e1a4488f9fdc8a742e2f36865ac31
Hazel Pedemonte <epedemont@protonmail.ch>
2017-11-06 21:20:12 -0800

    Try raspi-install.sh again

--- raspi-install.sh
+++ raspi-install.sh
@@ -3,17 +3,15 @@ wget -O Whiskers.image https://raw.githubusercontent.com/NoMod-Programming/Whisk
 mv Whiskers.image /usr/share/scratch/Whiskers.image
 cp /usr/bin/scratch /usr/bin/whiskers
 sed -i 's#IMAGE=.*#IMAGE="/usr/share/scratch/Whiskers.image"#' /usr/bin/whiskers
-cat > /home/$(logname)/Desktop/whiskers.desktop << EOL
-line 1, [Desktop Entry]
-line 2, Encoding=UTF-8
-line 3, Version=1.0
-line 4, Type=Application
-line 5, Exec=nuwhiskers
-line 6, Icon=scratch
-line 7, Terminal=false
-line 8, Name=Whiskers
-line 9, Comment=Programming system and content development tool
-line 10, Categories=APplication;Education;Development;
-line 11, MimeType=application/x-scratch-project
-EOL
+echo "[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Exec=nuwhiskers
+Icon=scratch
+Terminal=false
+Name=Whiskers
+Comment=Programming system and content development tool
+Categories=APplication;Education;Development;
+lMimeType=application/x-scratch-project" > /home/$(logname)/Desktop/whiskers.desktop
 chmod a+x /home/$(logname)/Desktop/whiskers.desktop
